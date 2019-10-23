b0f19efe7ddcd0e0f4facdd9fae90c3f29119908
Ranadeep Mitra <ranadeep@umich.edu>
2019-10-23 14:41:57 -0400

    Add test for subtraction

--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,6 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = pythonTest.calculate("1 1 +")
         self.assertEqual(2, result)
+    def test_sub(self):
+        result = pythonTest.calculate("2 1 -")
+        self.assertEqual(1, result)
