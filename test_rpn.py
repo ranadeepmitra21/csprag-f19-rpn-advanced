f64dcc33d025fd4382b78e68f68cd29853b12fee
Ranadeep Mitra <ranadeep@umich.edu>
2019-10-30 14:10:40 -0400

    Added multiplication test

--- test_rpn.py
+++ test_rpn.py
@@ -8,6 +8,9 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = pythonTest.calculate("2 1 -")
         self.assertEqual(1, result)
+    def test_mul(self):
+        result = pythonTest.calculate("4 8 *")
+        self.assertEqual(32, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             pythonTest.calculate('1 2 3 +')
