5b1efc1d4bca9b6185d9b8942c210dcd0ec98ad3
Ranadeep Mitra <ranadeep@umich.edu>
2019-10-30 14:38:55 -0400

    Renamed pythonTest to rpn
    The file was originally misnamed. This has now been rectified and
    test_rpn has updated to reference the correct file.

--- test_rpn.py
+++ test_rpn.py
@@ -1,22 +1,1 @@
-import unittest
-import pythonTest
-
-class TestBasics(unittest.TestCase):
-    def test_add(self):
-        result = pythonTest.calculate("1 1 +")
-        self.assertEqual(2, result)
-    def test_sub(self):
-        result = pythonTest.calculate("2 1 -")
-        self.assertEqual(1, result)
-    def test_mul(self):
-        result = pythonTest.calculate("4 8 *")
-        self.assertEqual(32, result)
-    def test_div1(self):
-        result = pythonTest.calculate("5 3 /")
-        self.assertEqual(1, result)
-    def test_div2(self):
-        result = pythonTest.calculate("12 3 /")
-        self.assertEqual(4, result)
-    def test_badinput(self):
-        with self.assertRaises(TypeError):
-            pythonTest.calculate('1 2 3 +')
+import unittestimport rpnclass TestBasics(unittest.TestCase):    def test_add(self):        result = rpn.calculate("1 1 +")        self.assertEqual(2, result)    def test_sub(self):        result = rpn.calculate("2 1 -")        self.assertEqual(1, result)    def test_mul(self):        result = rpn.calculate("4 8 *")        self.assertEqual(32, result)    def test_div1(self):        result = rpn.calculate("5 3 /")        self.assertEqual(1, result)    def test_div2(self):        result = rpn.calculate("12 3 /")        self.assertEqual(4, result)    def test_badinput(self):        with self.assertRaises(TypeError):            rpn.calculate('1 2 3 +')
